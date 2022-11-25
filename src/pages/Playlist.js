5f1b968a579f4e1385557c0ddbd09bf698768f8e
DeVon DuPree <devondupree@airdevonnypooh.attlocal.net>
2022-11-24 19:44:54 -0800

    start recs

--- src/pages/Playlist.js
+++ src/pages/Playlist.js
@@ -13,6 +13,8 @@ const Playlist = () => {
     const [token, setToken] = useState("")
     const [playlists, setPlaylists] = useState({selectedPlaylist: '', listOfPlaylists: []});
     const [cover, setCover] = useState("")
+    const [tracks, setTracks] = useState([])
+    const [recommendations, setRecommendations] = useState([])
 
     useEffect(() => {
       const hash = window.location.hash
@@ -32,14 +34,13 @@ const Playlist = () => {
         })
         .then (playlistResponse => {        
           setPlaylists({
-            selctedPlaylist: playlists.selectedPlaylist,
+            selectedPlaylist: playlists.selectedPlaylist,
             listOfPlaylists: playlistResponse.data.items
           })
         });
-    }, [playlists.selectedPlaylist])
+    }, [playlists.selectedPlaylist, spotify.ClientId, spotify.ClientSecret])
 
     const playlistChanged = val => {
-      console.log(val);
       setPlaylists({
         selectedPlaylist: val,
         listOfPlaylists: playlists.listOfPlaylists
@@ -56,9 +57,50 @@ const Playlist = () => {
         }
       })
       .then(coverResponse => {
-        console.log(coverResponse)
         setCover(coverResponse)
       });
+
+      axios(`https://api.spotify.com/v1/playlists/${playlists.selectedPlaylist}/tracks`, {
+        method: 'GET',
+        headers: {
+          'Authorization' : 'Bearer ' + token
+        }
+      })
+      .then(trackResponse => {
+        setTracks(trackResponse)
+      });
+
+      //track ids
+      // for(let i = 0; i < tracks.data.items.length; i++){
+      //   console.log(tracks.data.items[i].track.id)
+      // }
+
+      // artist ids
+      // console.log(tracks.data.items[0].track.artists[0].id)
+      
+      // for(let i = 0; i < tracks.data.items.length; i++){
+      //   for(let j = 0; j < tracks.data.items[i].track.artists.length; j++){
+      //     console.log(tracks.data.items[i].track.artists[j].name)
+      //   }
+      // }
+
+      axios('https://api.spotify.com/v1/recommendations', {
+        method: 'GET',
+        headers: {
+          'Authorization' : 'Bearer ' + token
+        },
+        params: {
+          seed_artists: "4NHQUGzhtTLFvgF5SZesLK", //array of up to 5 seed artist ids
+          seed_genres: "hip-hop", //array of up to 5 seed genre names
+          seed_tracks: "0c6xIDDpzE81m2q797ordA", //array of up to 5 seed track ids
+        }
+      })
+      .then(recResponse => {
+        setRecommendations(recResponse)
+      });
+
+      console.log(recommendations)
+
     }
 
     return(
@@ -75,6 +117,8 @@ const Playlist = () => {
                 <button className="bg-white text-[2.118vw] px-[3.125vw] p-[0.313vw] rounded-lg text-black" type={"submit"}>Submit</button>
               </form>
             </div>
+            {cover ?
+            <img src={cover.data[0].url} alt="Playlist Cover" /> : null}
       </div>
     )
 }
